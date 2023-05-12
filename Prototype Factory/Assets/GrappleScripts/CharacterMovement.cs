aa990022c59dab02bee596af06c66fa4e9e8948b
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-05-12 18:17:03 -0400

    grappling gun prototype
    Different grapple modes on left and right click

--- Prototype Factory/Assets/GrappleScripts/CharacterMovement.cs
+++ Prototype Factory/Assets/GrappleScripts/CharacterMovement.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Rigidbody))]
 
 public class CharacterMovement : MonoBehaviour
 {
-    [Range(0, 25)] public float Speed = 1;
-    CharacterController Controller;
+    [Range(0, 50)] public float Speed = 15;
+    [HideInInspector] public bool InAir = false;
+    [HideInInspector] public Rigidbody RB;
     private void Awake()
     {
-        Controller = GetComponent<CharacterController>();
+        RB = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
-        Controller.Move(transform.rotation * Vector3.right * Speed * Input.GetAxisRaw("Horizontal")/100);
-        Controller.Move(transform.rotation * Vector3.forward * Speed * Input.GetAxisRaw("Vertical")/100);
-       
+        if (InAir)
+            return;
+  
+        Vector3 Vec = Vector3.zero;
+        Vec += transform.rotation * Vector3.right  * Input.GetAxisRaw("Horizontal");
+        Vec += transform.rotation * Vector3.forward * Input.GetAxisRaw("Vertical");
+        RB.velocity += Vec;
+        if (RB.velocity.magnitude > Speed)
+            RB.velocity = RB.velocity.normalized * Speed;
     }
 }
