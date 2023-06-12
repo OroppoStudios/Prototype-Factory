672e3188ff289bd80c159b39b38a17a4b149f811
Purple7989 <kai.joseph1@ontariotechu.net>
2023-06-12 18:49:05 -0400

    Installed Visual Effects Graph & Added Speed Lines

--- 3rd Person Prototype/Assets/SpeedUI.cs
+++ 3rd Person Prototype/Assets/SpeedUI.cs
@@ -8,16 +8,27 @@ public class SpeedUI : MonoBehaviour
 {
     private TextMeshProUGUI text;
     public Rigidbody RB;
+
+    public GameObject Speedlines;
     // Start is called before the first frame update
     private void Awake()
     {
         text = GetComponent<TextMeshProUGUI>();
+        Speedlines.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         text.text = (RB.velocity.magnitude * 3.6f).ToString("0.0") + " KMPH";
+        if(RB.velocity.magnitude * 3.6f > 73.0)
+        {
+            Speedlines.SetActive(true);
+        }
+        else
+        {
+            Speedlines.SetActive(false);
+        }
     
     }
 }
