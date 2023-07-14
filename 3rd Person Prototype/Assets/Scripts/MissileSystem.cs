31b1cbe0d5f969435ad63b51f6193702589abd92
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-07-14 19:38:51 -0400

    dynamic target acquisition

--- 3rd Person Prototype/Assets/Scripts/MissileSystem.cs
+++ 3rd Person Prototype/Assets/Scripts/MissileSystem.cs
@@ -12,8 +12,10 @@ public class MissileSystem : MonoBehaviour
     {
         new Vector2(-160.0f,90.0f), new Vector2(-160.0f,-90.0f), new Vector2(160.0f,-90.0f),new Vector2(160.0f,90.0f)
     };
-   protected const float InitFOV = 28.0f;
+    protected const float InitFOV = 28.0f;
+    public LayerMask WhatIsTargetable;
     [Range(0,1)] public float TargetingSystemSize=1;
+    [Range(0, 150)] public float MissileSeekRange = 50f;
     public List<RectTransform> TargetingCorners;
    
 
@@ -28,18 +30,47 @@ public class MissileSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Tracking = (IsVisible(Camera, Target.gameObject));
-                  
-       TargetIndicator.SetActive(Tracking);
-           
-       TargetIndicator.GetComponent<RectTransform>().anchoredPosition =
-               new Vector2((Camera.WorldToViewportPoint(Target.position).x- 0.5f ) * 320.0f,
-               (Camera.WorldToViewportPoint(Target.position).y - 0.5f) * 180.0f); ;
+        Collider[] Targets = Physics.OverlapSphere(transform.position, MissileSeekRange, WhatIsTargetable);
+        Target = GetClosestEnemy(Targets);
+        if(Target)
+            TryTarget(Target);
 
-        
-        
+      
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, MissileSeekRange);
+    }
+    Transform GetClosestEnemy(Collider[] enemies)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (Collider potentialTarget in enemies)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget.transform;
+            }
+        }
 
+        return bestTarget;
+    }
+    void TryTarget(Transform T)
+    {
+        Tracking = (IsVisible(Camera, T.gameObject));
+
+        TargetIndicator.SetActive(Tracking);
+
+        TargetIndicator.GetComponent<RectTransform>().anchoredPosition =
+                new Vector2((Camera.WorldToViewportPoint(T.position).x - 0.5f) * 320.0f,
+                (Camera.WorldToViewportPoint(T.position).y - 0.5f) * 180.0f); ;
+
+    }
     private bool IsVisible(Camera c, GameObject target)
     {
         var planes = GeometryUtility.CalculateFrustumPlanes(c);
